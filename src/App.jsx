a981f9f4909df19b14d463307c6ed5a293daf473
Carlos Echavez <carlosechavezjr@gmail.com>
2023-08-22 11:38:46 +0800

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import Login from "./Login";
+import Register from "./Register";
+
+function App() {
+  const [currentForm, setCurrentForm] = useState("login");
+
+  const toggleForm = (forName) => {
+    setCurrentForm(forName);
+  };
+
+  return (
+    <div className="App">
+      {currentForm === "login" ? (
+        <Login onFormSwitch={toggleForm} />
+      ) : (
+        <Register onFormSwitch={toggleForm} />
+      )}
+    </div>
+  );
+}
+
+export default App;
